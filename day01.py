7bc253d56f2adcc64e012092304c0f3f63d0496a
Atvaark <Atvaark@users.noreply.github.com>
2017-12-01 13:58:19 +0100

    Day01 2/2

--- day01.py
+++ day01.py
@@ -4,17 +4,33 @@ def sum_digits(d1, d2):
 
     return 0
 
-def sum_input(input):
-    if len(input) <= 1:
+def sum_neighbor(numbers):
+    if len(numbers) <= 1:
         return 0
     
     sum = 0
-    for i in range(0, len(input)):
-        if i < len(input) - 1:
-            sum += sum_digits(input[i], input[i+1])
+    for i in range(0, len(numbers)):
+        if i < len(numbers) - 1:
+            sum += sum_digits(numbers[i], numbers[i+1])
 
-    if len(input) > 2:
-        sum += sum_digits(input[0], input[-1:])
+    if len(numbers) > 2:
+        sum += sum_digits(numbers[0], numbers[-1:])
+
+    return sum
+
+def sum_halfway(numbers):
+    if len(numbers) <= 1:
+        return 0
+    
+    if len(numbers) % 2 > 0:
+        return 0
+
+    sum = 0
+    for i in range(0, len(numbers)):
+        j = (int(len(numbers)/2)+i)%len(numbers)
+
+        if i != j:
+            sum += sum_digits(numbers[i], numbers[j])
 
     return sum
 
@@ -26,14 +42,22 @@ def main():
         out_lines = f.read().splitlines()
 
     for n in range(0, len(in_lines)):
-        in_line = in_lines[n]
+        in_line = in_lines[n].split(" ")
+        in_part = in_line[0]
+        in_number = in_line[1]
+
         out_line = int(out_lines[n])
 
-        actual = sum_input(in_line)
+        if in_part == '1':
+            actual = sum_neighbor(in_number)            
+        elif in_part == '2':
+            actual = sum_halfway(in_number)
+        else:
+            actual = -1
 
         match = out_line == actual
-
-        print (match)
+        
+        print('{} {} {} {}'.format(n+1, in_part, actual, match))
 
 if __name__ == '__main__':
     main()
